6b428bc78f7423208c1460552f253458552cee43
youssefadly <youssefadly332@gmail.com>
2025-06-17 15:49:42 +0300

    add CustomAppBar in NotesViewBody

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_search_Icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 50 ),
+      child: Row(
+        spacing: 245,
+        children: [
+          Text('Notes',style: TextStyle(fontSize: 28),),
+        CustomSearchIcon()
+      ],),
+    );
+  }
+}
