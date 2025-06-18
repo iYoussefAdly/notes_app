ce57ce2a622a537aadf415456dd2a93515d2b9e1
youssefadly <youssefadly332@gmail.com>
2025-06-18 18:35:08 +0300

    finish NoteBottomSheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,19 +9,29 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(15),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 38),
-            child: CustomTextField(
-              hint: 'Title',
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 38),
+              child: CustomTextField(
+                hint: 'Title',
+              ),
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          CustomTextField(hint: 'Content',maxLines: 6,)
-        ],
+            SizedBox(
+              height: 30,
+            ),
+            CustomTextField(hint: 'Content',maxLines: 6,
+            ),
+            SizedBox(
+              height: 70,
+            ),
+            CustomButton(
+              onTap: () {              
+              },
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.onTap});
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(color: kPrimaryColor),
+        width: MediaQuery.of(context).size.width,
+        height: 55,
+        child: Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
